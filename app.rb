74f6493dfa03f74c2e16737a8cc574ac8235488f
BreadMan96 <breadman.v96@gmail.com>
2020-04-08 13:18:29 +0300

    Add cart page, cart removal

--- app.rb
+++ app.rb
@@ -4,6 +4,27 @@ require 'sinatra/reloader'
 require 'sqlite3'
 require 'sinatra/activerecord'
 
+def parse_orders_input orders_input
+  s1 = orders_input.split(/,/)
+
+  arr = []
+
+  s1.each do |x|
+    s2 = x.split(/\=/)
+
+    s3 = s2[0].split(/_/)
+
+    id = s3[1]
+    cnt = s2[1]
+
+    arr2 = [id, cnt]
+
+    arr.push arr2
+  end
+
+  return arr
+end
+
 set :database, "sqlite3:hookah_inspection.db"
 
 class Hookah < ActiveRecord::Base
@@ -26,3 +47,24 @@ end
 get '/question-answer' do
 	erb :question_answer
 end
+
+post '/cart' do
+  @orders_input = params[:orderstring]
+  @items = parse_orders_input @orders_input
+
+  # выводим сообщение о том, что корзина пуста
+  if @items.length == 0
+    return erb :cart_is_empty
+  end
+
+  @items.each do |item|
+    # id, cnt
+    item[0] = Hookah.find(item[0])
+  end
+
+	erb :cart
+end
+
+
+
+
